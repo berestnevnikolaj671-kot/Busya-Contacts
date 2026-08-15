40252880b184f534643c275f32f14fce5e8f85c5
BusyaProject <berestnevnikolaj671@gmail.com>
2026-08-15 10:39:29 +0300

    Add files via upload

---
+++ AppConfig.kt
@@ -0,0 +1,23 @@
+package com.webtoapp.generated
+
+object AppConfig {
+    const val APP_NAME = "BusyaContacts (32 bit)"
+    const val TARGET_URL = ""
+
+    const val ACTIVATION_ENABLED = false
+    val ACTIVATION_CODES = listOf()
+
+    const val AD_BLOCK_ENABLED = false
+    val AD_BLOCK_RULES = listOf()
+
+    const val ANNOUNCEMENT_ENABLED = false
+    const val ANNOUNCEMENT_TITLE = ""
+    const val ANNOUNCEMENT_CONTENT = ""
+    const val ANNOUNCEMENT_LINK = ""
+    const val ANNOUNCEMENT_SHOW_ONCE = true
+
+    const val JAVASCRIPT_ENABLED = true
+    const val DOM_STORAGE_ENABLED = true
+    const val ZOOM_ENABLED = true
+    const val DESKTOP_MODE = false
+}
